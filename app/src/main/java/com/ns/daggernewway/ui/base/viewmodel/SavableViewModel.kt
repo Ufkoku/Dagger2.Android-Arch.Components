0ee5d605abee9a87a7fe76a7d5b2baf5ddbdc010
Ufkoku <nas8717@gmail.com>
2018-08-16 14:30:34 +0300

    Improvement.

--- app/src/main/java/com/ns/daggernewway/ui/base/viewmodel/SavableViewModel.kt
+++ app/src/main/java/com/ns/daggernewway/ui/base/viewmodel/SavableViewModel.kt
@@ -4,21 +4,39 @@ import android.app.Application
 import android.os.Bundle
 import androidx.lifecycle.AndroidViewModel
 
-abstract class SavableViewModel(app: Application) : AndroidViewModel(app) {
+abstract class SavableViewModel(app: Application,
+                                bundleSuffix: String = "") : AndroidViewModel(app) {
+
+    /**
+     * Allows you to use two models of same instance attached to single component
+     * */
+    @Suppress("CanBePrimaryConstructorProperty")
+    private val bundleSuffix = bundleSuffix
+
+    private val bundleKey: String by lazy {
+        val basicKey = javaClass.canonicalName!!
+        if (this.bundleSuffix.isEmpty()) {
+            basicKey
+        } else {
+            "$basicKey$${this.bundleSuffix}"
+        }
+
+    }
 
     constructor(app: Application,
-                savedInstanceState: Bundle) : this(app) {
+                savedInstanceState: Bundle,
+                bundleSuffix: String = "") : this(app, bundleSuffix) {
         restore(savedInstanceState)
     }
 
     fun save(bundle: Bundle) {
         val innerBundle = Bundle()
         saveInner(innerBundle)
-        bundle.putBundle(javaClass.canonicalName, innerBundle)
+        bundle.putBundle(bundleKey, innerBundle)
     }
 
     private fun restore(bundle: Bundle) {
-        val innerBundle = bundle.getBundle(javaClass.canonicalName)
+        val innerBundle = bundle.getBundle(bundleKey)
         restoreInner(innerBundle!!)
     }
 
